61d19efb2b4b8746583337109afe792e98643093
Sharonee <35001043+Sharonee@users.noreply.github.com>
2018-01-01 15:12:56 +0200

    Added PY files

---
+++ section1_VairblesAndFunctions.py
@@ -0,0 +1,120 @@
+#User Input
+
+#greeting2 = input("Write a greeting: ")
+#print(greeting2)
+
+
+
+
+
+#what is the type?
+
+#print(type(greeting2))
+
+
+
+
+
+#How to convert Strint to Int
+
+#age = input("Enter your age: ")
+#new_age = int(age) + 50
+#print(new_age)
+
+
+#If you want to convert to String  str(50)
+
+
+
+
+
+#String
+# To see all the sting operations  dir("")
+
+#c = "Here"
+#print(c.replace("e","i"))
+
+#print(c.upper())
+
+#print(dir(""))
+
+
+
+
+
+#String Indexing and Splitting
+
+#c = "Hi There!"
+#print(c[3])  #This command will print the lecture in index 3 = T
+
+#print(c[-1]) #will print the index -1 from the end = !
+
+
+#print(c[0:2]) # Will print index from 0-1 = Hi
+
+
+
+
+
+
+
+
+
+
+
+#List
+
+#c= ["H", 2, "Hello"]
+#print(c)
+
+
+#print(c[1])  #list index in place 1  will print "2"
+
+#print(dir(list))   #Print the list options
+
+#c.append(3)  #Add number "3" to the end of the list
+#print(c)
+
+#Tuple - kind of list but there are mutable (cannot change them)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Dictioanries
+
+
+
+d = {"name":"John", "Surname":"Smith"}   #name is the key, surname is the value
+print(d["name"])
+
+
+e = {"name":"John", "Surname":"Smith","Cities":("Telaviv","Haifa","Jerusalem")}  #Here we have 3 values in cities key
+print(e["Cities"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
